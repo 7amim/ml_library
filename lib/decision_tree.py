e3a49aa9752a227ae9b2d853761eb75e56145ea9
Tamim <tufaruk7@gmail.com>
2024-08-29 15:19:37 -0400

    Added a field for max_depth as well as some comments.

--- lib/decision_tree.py
+++ lib/decision_tree.py
@@ -46,33 +46,37 @@ import numpy as np
 class Node:
 
     def __init__(self,
-                 feature,
-                 target,
-                 label):
+                 feature: list | np.array,
+                 target: list | np.array,
+                 label: int,
+                 feature_idx: int):
 
         self.feature: np.array = feature
+        self.categories: set = set(self.feature)
         self.target: np.array = target
         self.label = label
 
-        self.impurity = None
+        self.total_samples = len(~np.isnan(self.feature))
 
-        self.left: Node = None
-        self.right: Node = None
+        self.impurity = None
+        self.depth = None # not organized in tree, so no depth
+
+        self.branches = dict(self.categories)
 
     @staticmethod
     def gini_impurity(num_yes: int, num_no: int) -> float:
-        return 1 - num_yes / (num_yes + num_no) - num_no / (num_yes + num_no)
+        return 1 - np.square(num_yes / (num_yes + num_no)) - np.square(num_no / (num_yes + num_no))
 
     def weighted_impurity(self):
-
-        impurities: dict[int] = {}
-        proportions: dict[int] = {}
-
-        categories = np.unique(self.feature)
-
+        """
+        Sets the total impurity for node based on the weighted sum of the impurity of each
+        category.
+        """
         weighted_impurity = 0
 
-        for c in categories:
+        self.category_impurity = {}
+
+        for c in self.categories:
             # determine the number of yes and no votes for each category
             num_yes = np.sum((self.feature == c) & (self.target == self.label))
 
@@ -81,16 +85,92 @@ class Node:
             if num_yes + num_no == 0:
                 continue
 
-            num_c = np.sum((self.feature == c))
+            num_c = num_yes + num_no
 
+            c_impurity = Node.gini_impurity(num_yes, num_no)
+
+            # stored for future use when creating splits in a decision tree
+            self.category_impurity[c] = c_impurity
             # calculate the impurity associated with each category and weight it by its proportion in the feature
-            weighted_impurity += (num_c / len(self.feature)) * Node.gini_impurity(num_yes, num_no)
+            weighted_impurity += (num_c / len(self.feature)) * c_impurity
 
         self.impurity = weighted_impurity
 
+    def set_depth(self, depth: int):
+        self.depth = depth
 
 
-# class decision_tree:
-#
-#     def __init__(self):
-#
+class DecisionTree:
+
+    def __init__(self, features, target, label, feature_names = None, max_depth: int = 5):
+        self.root = None
+
+        self.features = features
+        self.target = target
+        self.label = label
+
+        self.feature_names = feature_names if feature_names else np.arange(len(self.features))
+
+        self.feature_nodes = {}
+
+        self.max_depth = max_depth
+
+    def determine_root(self):
+        # find the root node by identifying the node with the minimum impurity
+
+        min_impurity = 1
+        root = None
+
+        for idx, feature in enumerate(self.features):
+
+            feature_node = Node(self.features[:, idx], self.target, self.label)
+
+            curr_impurity = feature_node.weighted_impurity()
+
+            if curr_impurity < min_impurity:
+                root = feature_node
+                min_impurity = curr_impurity
+
+        return root
+
+    def fit(self):
+
+        self.root = self.determine_root()
+
+        self.root.set_depth(1)
+
+        self.split_node(self.root)
+
+
+    def split_node(self, node):
+
+        if node.depth >= self.max_depth:
+            return
+
+        for c in node.categories:
+
+            if node.category_impurity[c] > 0:
+                # find the indices of where the current category is active
+                subset_indices  = np.where(self.features[:, node.feature_idx] == c)
+                # index the remaining features based on the category
+                remaining_features = self.features[subset_indices][:,]
+
+                min_impurity = 1
+                best_child_node = None
+
+                # traverse the other features to find the next feature node based on minimum impurity
+                for feature in remaining_features.T:
+
+                    child_node = Node(feature, self.target, feature, node.feature_idx)
+                    child_node.weighted_impurity()
+
+                    if child_node.impurity < min_impurity:
+                        min_impurity = child_node.impurity
+                        best_child_node = child_node
+
+                best_child_node.set_depth(node.depth + 1)
+                node.branches[c] = best_child_node
+
+                # repeat the process
+                self.split_node(best_child_node)
+
