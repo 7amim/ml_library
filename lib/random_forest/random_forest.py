131dbe996d8504a16cbeaf45c44509636dbecea3
Tamim <tufaruk7@gmail.com>
2024-09-04 01:16:47 -0400

    Fixed a typo for the random forest class.

--- lib/random_forest/random_forest.py
+++ lib/random_forest/random_forest.py
@@ -28,7 +28,7 @@ class RandomForest:
         self.n_estimators = n_estimators
         self.n_features = n_features
         self.max_depth = max_depth
-        self.min_samples_split = min_samples_split\
+        self.min_samples_split = min_samples_split
         self.random_state = random_state
 
         # Setting random seed for reproducibility
