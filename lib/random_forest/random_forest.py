bd39945c06db60c274af56a73f527f32d073ae5b
Tamim <tufaruk7@gmail.com>
2024-09-04 21:52:12 -0400

    Added doc strings to the RandomForest class.

--- lib/random_forest/random_forest.py
+++ lib/random_forest/random_forest.py
@@ -1,22 +1,3 @@
-"""
-Steps
-
-1. Create a bootstrapped dataset in which the same sample can appear twice in the bootstrapped dataset, as long as it
-is the same length as the original dataset
-
-2. Create a decision tree using the bootstrapped dataset - specifically a random subset of a certain length
- of the dataset
-
-3. Do step 1 and 2 many times
-
-4. We can use the consensus of the decision trees to predict a class
-
-5. We use the out-of-bag error, calculated on the samples that did not enter the decision tree consideration,
-to evaluate the accuracy of the forest
-
-6. Then we can do step 1 and 2 for a new number of variables, i.e. create a random forest again, redo steps 1-5 and
-compare the out-of-bag error against the previous out-of-bag error
-"""
 from lib.decision_tree.decision_tree import  DecisionTree
 from scipy.stats import mode
 
@@ -25,6 +6,15 @@ import numpy as np
 class RandomForest:
     def __init__(self, n_estimators: int = 100, n_features: int = 2, max_depth: int = 100, min_samples_split: int = 2,
                  random_state: int = 42):
+        """
+        Creates a RandomForest classifier that is based on the DecisionTree class.
+
+        :param n_estimators: the number of trees in the forest
+        :param n_features: the number of features to select from for each tree
+        :param max_depth: the maximum depth of the tree
+        :param min_samples_split: the minimum number of samples required to split a node
+        :param random_state: used to reproduce the results
+        """
         self.n_estimators = n_estimators
         self.n_features = n_features
         self.max_depth = max_depth
@@ -37,7 +27,14 @@ class RandomForest:
 
         self.trees = []
 
-    def fit(self, features, target, label):
+    def fit(self, features, target, label=1):
+        """
+        Given the features and target labels, fits the trees to the data.
+
+        :param features: the training feature set
+        :param target: the training target set
+        :param label: the target label
+        """
         for i in range(self.n_estimators):
             bootstrapped_features, bootstrapped_target = RandomForest.bootstrap_features(features, target)
             selected_features = RandomForest.select_features(bootstrapped_features, self.n_features)
@@ -47,16 +44,36 @@ class RandomForest:
 
     @staticmethod
     def bootstrap_features (features, target):
+        """
+        Returns the bootstrapped dataset.
+
+        :param features: the original training feature set
+        :param target: the original target set
+        :return: the bootstrapped dataset with its labels
+        """
         rows, cols = features.shape
         bootstrap_index = np.random.choice(rows, rows, replace=True)
         return features[bootstrap_index], target[bootstrap_index]
 
     @staticmethod
     def select_features(features, n_features):
+        """
+        Selects the number of features to use when generating a given tree.
+
+        :param features: the training feature set
+        :param n_features: the number of features to choose from
+        :return:
+        """
         cols = features.shape[1]
         feature_indices = np.random.choice(cols, n_features, replace=False)  # Randomly select column indices
         return features[:, feature_indices]
 
     def predict(self, features):
+        """
+        Returns the predictions for a given set of features.
+
+        :param features: the features to perform prediction on
+        :return: the labels for each input feature
+        """
         predictions = np.array([tree.predict(features) for tree in self.trees])
         return mode(predictions, axis=0).mode[0]
--- lib/random_forest/train.py
+++ lib/random_forest/train.py
@@ -13,7 +13,7 @@ dataset = datasets.load_breast_cancer()
 X_train, X_test, y_train, y_test = train_test_split(dataset.data,
                                                     dataset.target,
                                                     test_size=0.2,
-                                                    random_state=0)
+                                                    random_state=1234)
 
 classifier = RandomForest()
 classifier.fit(X_train, y_train, 1)
